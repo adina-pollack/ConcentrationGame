f0becdbb233faed54689357bc7a801e5ccbfefad
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-06 12:49:30 -0400

    Added random to create matching pairs for card divs

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 var $organize = $("#organize")
-for(var i = 0; i < 8; i++){
+for(var i = 0; i < 4; i++){
   var $cardNum = $('<div>').addClass("card").val(i+1).text(i+1);
   $organize.append($cardNum);
 }
+// console.log(i); Testing Value of i
+var combos = (i / 2)-1;
+function random(){
+  return Math.round(Math.random()*(combos)+1)
+}
+
+// for(var k = 0; k < 1000; k++){
+// console.log(random());
+// } Tester code to se value of random function, next to assign these values to cards to create matching pairs
+
 
 $(".card").on("click",function(){
   $(this).css("background-color","blue");
@@ -10,5 +20,5 @@ $(".card").on("click",function(){
   console.log($(this).val());
   // if(($(this).val())==1){
   //   $(this).css("background-color","yellow");
-  // } Tester Code
+  // } Tester Code to check for values on different "card divs"
 })
