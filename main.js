ca4ff281052536c8d5084648a8bd631b6bba2433
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-06 12:03:22 -0400

    Assigned Cards a Value :)

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-$(".card").on("click",function(){ $(this).css("background-color","blue"); console.log("Hello"); })
+var $organize = $("#organize")
+for(var i = 0; i < 8; i++){
+  var $cardNum = $('<div>').addClass("card").val(i+1).text(i+1);
+  $organize.append($cardNum);
+}
+
+$(".card").on("click",function(){
+  $(this).css("background-color","blue");
+  console.log("I'm Blue");
+  console.log($(this).val());
+  // if(($(this).val())==1){
+  //   $(this).css("background-color","yellow");
+  // } Tester Code
+})
