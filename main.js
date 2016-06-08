564da678bbd88dedc9614d914870d7f18cc66e76
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-08 09:30:01 -0400

    finally able set cards to match each other :)

--- main.js
+++ main.js
@@ -1,24 +1,75 @@
-var $organize = $("#organize")
-for(var i = 0; i < 4; i++){
-  var $cardNum = $('<div>').addClass("card").val(i+1).text(i+1);
-  $organize.append($cardNum);
+var cards = 16;
+var $grid = $("#grid")
+var potentialNumbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+potentialNumbers.sort(function(a, b){
+  var randomNumber = Math.random();
+  if(randomNumber < 0.5){
+    return 1;
+  }else{
+    return -1;
+  }
+});
+
+console.log(potentialNumbers)
+for(var i = 0; i < cards; i++){
+  var $cardNum = $('<div>').addClass("card").text(potentialNumbers[i]);
+  $grid.append($cardNum);
 }
-// console.log(i); Testing Value of i
 var combos = (i / 2)-1;
 function random(){
-  return Math.round(Math.random()*(combos)+1)
+  var number = Math.round(Math.random()*(combos)+1);
+   return number;
 }
+var topHalf = [];
+var bottomHalf = [];
+function startUp(){
+  for(var s = 0; s <=combos; s++){
+ if(topHalf.includes(random()) == false ){
+   topHalf[s] = random();
+ }
 
-// for(var k = 0; k < 1000; k++){
-// console.log(random());
-// } Tester code to se value of random function, next to assign these values to cards to create matching pairs
+  console.log(topHalf);
+//  console.log(bottomHalf);
 
+}
+}
+var cardText;
+console.log(cardText)
+$(".card").on("click",function card(){
+  console.dir($(this).text())
+  if(cardText == null){
+    cardText = $(this).text()
+  }else if(cardText == $(this).text()){
+    console.log("ITS A MATCH!!!!!!");
+    // console.log("cardText already has a value "+ cardText)
+  }
+  // if (cardText) {
+  //
+  // }var cardText = $(this).text();
+  // if ($(".card").text == cardText){
 
-$(".card").on("click",function(){
-  $(this).css("background-color","blue");
-  console.log("I'm Blue");
-  console.log($(this).val());
+  // console.log("I'm Blue");
+ // console.log($(this).text());
+
   // if(($(this).val())==1){
   //   $(this).css("background-color","yellow");
-  // } Tester Code to check for values on different "card divs"
+  // } Tester Code to check for values on different ".card divs"
 })
+
+
+
+// console.log("The value of combos: " + combos); testing value of combos variable
+// console.log(i); Testing Value of i
+
+
+
+
+//for(/** do this for the amount of blocks you have*/){
+  //assigns each card a random value that does not repeat
+
+
+//}
+// for(var k = 0; k < 1000; k++){
+// console.log("The value of random is: "+ random());
+// } Tester code to se value of random function, next to assign these values to cards to create matching pairs
